588cf0aab6efaaa246740342743ce6a48c9305b8
Liam Cottrell <liamcottrell1996@hotmail.co.uk>
2023-04-16 11:46:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -111,9 +111,12 @@ EMAIL_SERVER = "smtp.office365.com"  # Adjust server address, if you are not usi
 # envars = current_dir / ".env"
 # load_dotenv(envars)
 
-# Read environment variables
-sender_email = os.environ['USER_EMAIL']
-password_email = os.environ['USER_PASSWORD']
+# # Read environment variables
+# sender_email = os.environ['USER_EMAIL']
+# password_email = os.environ['USER_PASSWORD']
+
+sender_email = 'liamcottrell1996@hotmail.co.uk'
+password_email = 'Pitshanger'
 
 
 print(sender_email, password_email)
